3ecb8283188073a03afe8950e615a20fd5f1b500
Carlos Rodriguez <carlos1843216@gmail.com>
2020-05-19 12:53:34 -0500

    REST Server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 // Puerto
 // ================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ================================
+// Entorno
+// ================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ================================
+// Entorno
+// ================================
+
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe'
+}
+else{
+    urlDB = 'mongodb+srv://carlox98:pK2ng0CdMP4I1Gte@cluster0-yzlox.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,43 +1,24 @@
+// Requires
 require('./config/config')
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-//Middleware
+//Middlewares
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(require('./routes/usuarioRoutes'))
 
-app.get('/usuario', (req, res)=>{
-    res.json('Get Usuario');
-});
-
-app.post('/usuario', (req, res)=>{
-    let body = req.body;
-
-    if(body.nombre == undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
+// Conectarnos a la base de datos
+mongoose.connect(process.env.URLDB,
+{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
+,(err, res)=>{
+    if(err){
+        throw err;
     }
-    else{
-        res.json({
-            persona: body
-        });
-    }
-    
+    console.log('Base de datos Online')
 });
-
-app.put('/usuario/:id', (req, res)=>{
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res)=>{
-    res.json('Delete Usuario');
-});
-
 
 app.listen(process.env.PORT, ()=>{
     console.log('Escuchando el puerto 3000')
