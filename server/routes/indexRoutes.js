c214325c8685d4af1030a0a62fdda384994ed701
Carlos Rodriguez <carlos1843216@gmail.com>
2020-05-21 09:24:50 -0500

    Productos y Categorias

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.use(require('./usuarioRoutes'))
-app.use(require('./loginRoutes'))
-
+app.use(require('./usuarioRoutes'));
+app.use(require('./loginRoutes'));
+app.use(require('./categoriaRoutes'));
+app.use(require('./productoRoutes'));
 
 module.exports = app;
